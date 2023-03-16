a2d8105c777de46b85036056d474311e04b9121f
NoahNym <noahnym@gmail.com>
2023-03-16 08:27:59 +0200

    Added usernames and a reset canvas  with voting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import required modules
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -11,66 +12,146 @@ const io = require("socket.io")(server, {
     }
 });
 
+// Set up server to listen on port 3000
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
 
+// Serve the index.html file when the user navigates to the root URL
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/draw.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
+// Set up variables to keep track of votes and users who have voted
+const votedUsers = new Set();
+let votes = 0;
+
+// Set up socket.io connection and define event handlers
 io.on('connection', (socket) => {
+
+
+
+
+
+
+    // Calculate the threshold number of votes needed to reset the canvas
+    const threshold = Math.floor((io.engine.clientsCount / 2) + 1);
+
+    // Calculate the number of players currently online
+    const players = Math.ceil((io.engine.clientsCount));
+
+    // Emit the number of players online to all sockets
+    io.emit(`playersOnline`, players)
+
+    // Emit the threshold number of votes to all sockets
+    io.emit('votes threshold', threshold);
+
     // Assign username to socket ID
     socket.on('set username', (username) => {
         socket.username = username;
+
+        // Emit a message indicating that the user has joined the chat
         io.emit('chat message', `${socket.username} joined`);
     });
 
+    // Handle chat messages sent by the user
     socket.on('chat message', (msg) => {
-        // Send message with username
+
+        // Send message with username to all sockets
         io.emit('chat message', `${socket.username}: ${msg}`);
     });
 
+    // Handle disconnection events
     socket.on('disconnect', () => {
+
+        // Recalculate the threshold number of votes needed to reset the canvas
+        const threshold = Math.floor((io.engine.clientsCount / 2) + 1);
+
+        // Emit the new threshold number to all sockets
+        io.emit('votes threshold', threshold)
+
+        // Calculate the number of players currently online
+        const players = Math.floor(io.engine.clientsCount);
+
+        // Emit the number of players online to all sockets
+        io.emit(`playersOnline`, players)
+
+        // send a message that a user disconnected and If the user had previously voted, remove their vote and emit a message to update the vote count and 
         io.emit('chat message', `${socket.username} disconnected`);
+        if (votedUsers.has(socket.id)) {
+            votedUsers.delete(socket.id);
+            if (votes > 0) {
+                votes--;
+            }
+            console.log(`Removed vote from ${socket.username}`);
+            io.emit('votes', votes);
+        }
     });
-});
 
-io.on('connection', (socket) => {
+    // Emit the current vote count to the newly connected user
+    socket.emit('votes', votes);
 
+    // When a user votes to reset the canvas, update the vote count and notify all connected users
+    socket.on('vote', () => {
+        if (io.engine.clientsCount == 1) {
+            // If the only user connected is the one who voted, reset the canvas immediately
+            io.emit('resetCanvas');
+            return;
+        }
+        if (votedUsers.has(socket.id)) {
+            // If the user had already voted, remove their vote and notify all connected users
+            console.log("User has already voted. Removing vote...");
+            votedUsers.delete(socket.id);
+            if (votes > 0) {
+                if (votes >= threshold) {
+                    console.log("Threshold already reached. Not removing vote.");
+                } else {
+                    votes--;
+                    io.emit('chat message', `${socket.username} removed their vote`)
+                }
+            }
+        } else {
+            // If the user has not voted yet, add their vote and notify all connected users
+            io.emit('chat message', `${socket.username} voted to reset the canvas`)
+            votedUsers.add(socket.id);
+            votes++; // Increment votes
+        }
+        console.log(`Votes: ${votes}/${io.engine.clientsCount}`);
+        io.emit('votes', votes);
+        if (votes >= threshold) {
+            // If the vote count reaches the threshold, reset the canvas and notify all connected users
+            console.log("Threshold reached!");
+            votes = 0;
+            io.emit('resetCanvas');
+            votedUsers.clear()
+            io.emit('chat message', `The canvas has been reset!`)
+        }
+    });
 
-
-
-
+    // When a user sends coordinate data, emit it to all connected sockets
     io.on("coord", (data) => {
         io.emit("coordStart", data)
     })
+
+    // When a user draws on the canvas, emit the drawing data to all connected sockets
     socket.on("draw", (data) => {
         io.emit("draw", data)
-
     })
 
+    // When a user sends a drawing width, emit it to all connected sockets
     socket.on("sendDrawWidth", width => {
         io.emit("sendDrawWidth", width)
     })
 
+    // When a user sends a color, emit it to all connected sockets
     socket.on("sendColor", color => {
         io.emit("sendColor", color)
     })
 
+    // When a user ends their drawing, emit the end coordinate data to all connected sockets
     socket.on("coordEnd", coord => {
         io.emit("coordEnd", coord)
     })
 
 });
 
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-io.of("/orders").on("connection", (socket) => {
-    socket.on("order:list", () => { });
-    socket.on("order:create", () => { });
-});
-
-io.of("/users").on("connection", (socket) => {
-    socket.on("user:list", () => { });
-});
